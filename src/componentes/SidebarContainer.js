9832fd742704886915b5c533a931227abf60252b
Carolinavellaneda <112986149+Carolinavellaneda@users.noreply.github.com>
2024-05-19 11:40:22 -0500

    Update SidebarContainer.js

--- src/componentes/SidebarContainer.js
+++ src/componentes/SidebarContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from './Menu';
 //import Logo from '../../node_modules/admin-lte/dist/img/AdminLTELogo.png';
 //import Logo from '.../../public/dist/img/AdminLTELogo.png';
-import Logo from '.../.../AdminLTELogo.png'
+import Logo from '.../.../src/AdminLTELogo.png'
 import { Link } from 'react-router-dom';
 
 const SidebarContainer = () => {
